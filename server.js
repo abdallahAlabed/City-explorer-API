788edf00547a1ed310bc72d41e9e37a96e18bb3f
abdallahAlabed <abdallah.s.alabd@gmail.com>
2021-06-16 03:15:53 +0300

    in progress

--- server.js
+++ server.js
@@ -1,32 +1,77 @@
 require("dotenv").config();
 const express = require("express");
+const weatherData = require("./assets/weather.json");
 const cors = require("cors");
-const weatherData = require("./data/weather.json");
-
+const axios = require("axios");
+const { response } = require("express");
 const server = express();
 server.use(cors());
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
+
+class Data {
+  constructor(item) {
+    (this.datetime = item.datetime),
+      (this.description = item.weather.description);
+  }
+}
+class MovieData {
+  constructor(item) {
+    this.title = item.title,
+      this.img = `https://image.tmdb.org/t/p/w500${item.poster_path}`
+  }
+}
 
 server.get("/", (req, res) => {
-  res.send("This is Home page");
+  res.send("test route");
+});
+
+server.get("/weatherAll", (req, res) => {
+  res.send(weatherData);
+});
+
+server.get("/weather2", (req, res) => {
+  const key = process.env.WEATHER_KEY;
+  let searchQuery = req.query.searchQuery;
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQuery}&key=${key}`;
+  axios.get(url).then((response) => {
+    let result = response.data.data.map((item) => {
+      return new Data(item);
+    });
+    res.send(result);
+  });
 });
 
 server.get("/weather", (req, res) => {
-  let lat = req.query.lat;
-  let lon = req.query.lon;
   let searchQuery = req.query.searchQuery;
+  let result = weatherData
+    .find((item) => {
+      if (searchQuery == item.city_name) {
+        return item;
+      }
+    })
+    .data.map((item) => {
+      return new Data(item);
+    });
+  res.send(result);
+});
 
-  if (lat == weatherData.lat && lon == weatherData.lon && searchQuery == weatherData.city_name) {
-    res.send(weatherData.data);
-  } else {
-    res.status(500).send("Error, city not found");
-  }
+server.get("/movies", (req, res) => {
+  const key = process.env.MOVIE_KEY;
+  let cityName = req.query.searchQuery;
+  let url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&region=${cityName}`;
+  axios.get(url).then((response) => {
+    console.log(response);
+    let result = response.data.results.map((item) => {
+      return new MovieData(item)
+    });
+    res.send(result);
+  });
 });
 
 server.get("*", (req, res) => {
-  res.status(404).send("Sorry, this page not found");
+  res.status(404).send("sorry, this page not found");
 });
 
 server.listen(PORT, () => {
-  console.log(`Listing on port: ${PORT}`);
+  console.log(`listen on port ${PORT}`);
 });
