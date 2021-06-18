77f2303a0b3fac0461b9066077829ad41c51abd8
abdallahAlabed <abdallah.s.alabd@gmail.com>
2021-06-18 22:29:05 +0300

    lab08 fixing the branchs

--- server.js
+++ server.js
@@ -8,27 +8,40 @@ const server = express();
 server.use(cors());
 const PORT = process.env.PORT;
 
+/////////////////////////////////////////////////////////////////////////
+
 class Data {
   constructor(item) {
     (this.datetime = item.datetime),
       (this.description = item.weather.description);
   }
 }
-// class MovieData {
-//   constructor(item) {
-//     this.title = item.title,
-//       this.img = `https://image.tmdb.org/t/p/w500${item.poster_path}`
-//   }
-// }
+
+/////////////////////////////////////////////////////////////////////////
+
+class MovieData {
+  constructor(item) {
+    this.title = item.title,
+      this.img = `https://image.tmdb.org/t/p/w500${item.poster_path}`
+      this.overview = item.overview,
+      this.vote_average = item.vote_average,
+      this.popularity = item.popularity
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////
 
 server.get("/", (req, res) => {
   res.send("test route");
 });
 
+/////////////////////////////////////////////////////////////////////////
+
 server.get("/weatherAll", (req, res) => {
   res.send(weatherData);
 });
 
+/////////////////////////////////////////////////////////////////////////
 server.get("/weather", (req, res) => {
   const key = process.env.WEATHER_KEY;
   const lat = req.query.lat;
@@ -39,8 +52,10 @@ server.get("/weather", (req, res) => {
       return new Data(item);
     });
     res.send(result);
-  });
+  }).catch((err)=> errorHandler(err,res))
 });
+
+/////////////////////////////////////////////////////////////////////////
 
 // server.get("/weather", (req, res) => {
 //   const lat = req.query.lat;
@@ -64,9 +79,30 @@ server.get("/weather", (req, res) => {
 //   }
 // });
 
+/////////////////////////////////////////////////////////////////////////
 
+server.get("/movies", (req, res) => {
+ 
+  const key = process.env.MOVIE_KEY;
+  let cityName = req.query.searchQuery;
 
+  let url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&region=${cityName}`;
+  axios.get(url).then((response) => {
+    console.log(response);
+    let result = response.data.results.map((item) => {
+      return new MovieData(item)
+    });
+    res.send(result);
+  }).catch((err)=> errorHandler(err,res))
+});
 
+/////////////////////////////////////////////////////////////////////////
+
+function errorHandler(err,res){
+res.status(500).send("somthing went wrong ",err);
+};
+/////////////////////////////////////////////////////////////////////////
 server.listen(PORT, () => {
   console.log(`listen on port ${PORT}`);
 });
+/////////////////////////////////////////////////////////////////////////
