b209871cb766c25c21d7fc6a4d1625e8b46a66b1
abdallahAlabed <abdallah.s.alabd@gmail.com>
2021-06-18 18:56:10 +0300

    lab07 fixing the branchs

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ class Data {
       (this.description = item.weather.description);
   }
 }
-class MovieData {
-  constructor(item) {
-    this.title = item.title,
-      this.img = `https://image.tmdb.org/t/p/w500${item.poster_path}`
-  }
-}
+// class MovieData {
+//   constructor(item) {
+//     this.title = item.title,
+//       this.img = `https://image.tmdb.org/t/p/w500${item.poster_path}`
+//   }
+// }
 
 server.get("/", (req, res) => {
   res.send("test route");
@@ -29,48 +29,42 @@ server.get("/weatherAll", (req, res) => {
   res.send(weatherData);
 });
 
-server.get("/weather2", (req, res) => {
-  const key = process.env.WEATHER_KEY;
-  let searchQuery = req.query.searchQuery;
-  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQuery}&key=${key}`;
-  axios.get(url).then((response) => {
-    let result = response.data.data.map((item) => {
-      return new Data(item);
-    });
-    res.send(result);
-  });
-});
+// server.get("/weather2", (req, res) => {
+//   const key = process.env.WEATHER_KEY;
+//   let searchQuery = req.query.searchQuery;
+//   let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQuery}&key=${key}`;
+//   axios.get(url).then((response) => {
+//     let result = response.data.data.map((item) => {
+//       return new Data(item);
+//     });
+//     res.send(result);
+//   });
+// });
 
 server.get("/weather", (req, res) => {
-  let searchQuery = req.query.searchQuery;
-  let result = weatherData
-    .find((item) => {
-      if (searchQuery == item.city_name) {
-        return item;
-      }
-    })
-    .data.map((item) => {
-      return new Data(item);
-    });
-  res.send(result);
+  const lat = req.query.lat;
+  const lon = req.query.lon;
+  console.log(weatherData);
+console.log(lat);
+console.log(lon);
+
+  if (lat && lon) {
+
+
+    try {
+      const reponseData = weatherData.data.map((obj) => new Data(obj));
+      res.json(reponseData);
+    }
+    catch (error) {
+      res.send(error.message);
+    };
+  } else {
+    res.send("please provide a proper lat and lon ");
+  }
 });
 
-server.get("/movies", (req, res) => {
-  const key = process.env.MOVIE_KEY;
-  let cityName = req.query.searchQuery;
-  let url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&region=${cityName}`;
-  axios.get(url).then((response) => {
-    console.log(response);
-    let result = response.data.results.map((item) => {
-      return new MovieData(item)
-    });
-    res.send(result);
-  });
-});
 
-server.get("*", (req, res) => {
-  res.status(404).send("sorry, this page not found");
-});
+
 
 server.listen(PORT, () => {
   console.log(`listen on port ${PORT}`);
